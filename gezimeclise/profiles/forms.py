b13a47929b480c0467fd700db14c8d456232cc06
tuna vargi <tuna40@gmail.com>
2013-06-26 00:46:18 +0300

    profile update page

--- gezimeclise/profiles/forms.py
+++ gezimeclise/profiles/forms.py
@@ -6,11 +6,8 @@ class ProfileUpdateForm(ModelForm):
 
     class Meta:
         model = GeziUser
-        exclude = ['access_token', 'facebook_name', 'facebook_profile_url',
-                   'gender', 'facebook_open_graph', 'new_token_required',
-                   'supports', 'username', 'is_active', 'date_joined',
-                   'is_staff', 'is_superuser', 'groups', 'user_permissions',
-                   'password', 'last_login', 'facebook_id', 'raw_data']
+        fields = ['first_name', 'last_name', 'email', 'image',
+                  'about_me', 'blog_url', 'causes', 'region', 'tags']
 
 
 class ReportForm(ModelForm):
