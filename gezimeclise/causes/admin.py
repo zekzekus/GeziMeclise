2eebdf735211c4d4ca8a9653a1af4a86c014efc6
onur mat <omat@gezgin.com>
2013-07-15 10:20:46 +0300

    causes admin update

--- gezimeclise/causes/admin.py
+++ gezimeclise/causes/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from gezimeclise.causes.models import Cause, Comments
 
 
-admin.site.register(Cause)
+class CauseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'region', 'is_active')
+    list_editable = ('is_active',)
+admin.site.register(Cause, CauseAdmin)
+
+
 admin.site.register(Comments)
